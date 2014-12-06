1d4cc8ef05915e9cd64f6ca54481197a205d9222
Josh Hofing <joshhofing@gmail.com>
2014-12-06 09:33:07 -0500

    Basic skeleton

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello, world"
+    return render_template('page.html')
 
 if __name__ == '__main__':
     app.run('0.0.0.0', debug=True)
---
+++ static/main.js
@@ -0,0 +1,6 @@
+function run() {
+    var src = document.getElementById('src');
+    var ctx = src.getContext('2d');
+    ctx.fillStyle = 'red';
+    ctx.fillRect(0, 0, 10, 10);
+}
